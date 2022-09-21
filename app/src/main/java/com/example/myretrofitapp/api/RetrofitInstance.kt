21ee200a8e5d775490e88df36eae3cd9fa2bcd0b
mahmoudibrahimmohammad <mahmoud.ibrahim.mohammad@rikstv.no>
2022-09-21 11:02:26 +0200

    refactoring retrofit instance

--- app/src/main/java/com/example/myretrofitapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/myretrofitapp/api/RetrofitInstance.kt
@@ -2,20 +2,17 @@ package com.example.myretrofitapp.api
 
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 
 private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 class RetrofitInstance {
-
-    private val retrofit by lazy {
+    // Lazy make sure that the instance will be created just when It's called
+    // This can be helpful to avoid creating an retrofit instance every time a component render
+    private val retrofit: MyApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
-            // we need to add converter factory to
-            // convert JSON object to Java object
             .build()
-    }
-
-    val myApi: MyApi by lazy {
-        retrofit.create(MyApi::class.java)
+            .create(MyApi::class.java)
     }
 }
